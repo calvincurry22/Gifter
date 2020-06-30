21e06ce7f29b69104cee919e39a27f3a617a08b8
calvincurry22 <calvincurry22@gmail.com>
2020-06-30 15:41:47 -0500

    initial commit

---
+++ Gifter/Repositories/UserProfileRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class UserProfileRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserProfileRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _context.UserProfile.ToList();
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return _context.UserProfile.FirstOrDefault(up => up.Id == id);
+        }
+
+        public void Add(UserProfile up)
+        {
+            _context.Add(up);
+            _context.SaveChanges();
+        }
+
+        public void Update(UserProfile up)
+        {
+            _context.Entry(up).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var userProfile = GetById(id);
+            _context.UserProfile.Remove(userProfile);
+            _context.SaveChanges();
+        }
+    }
+}
