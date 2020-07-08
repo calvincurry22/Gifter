448f4509dec7f498fe4f4a8a156b8310bf3b063d
calvincurry22 <calvincurry22@gmail.com>
2020-07-08 15:03:38 -0500

    Added firebase auth to startup.cs

--- Gifter/Models/UserProfile.cs
+++ Gifter/Models/UserProfile.cs
@@ -9,6 +9,9 @@ namespace Gifter.Models
         public int Id { get; set; }
 
         [Required]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
         [Required]
